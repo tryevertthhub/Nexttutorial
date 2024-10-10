defb614d391055495bafa5a402b42c513bafa646
tryevertthhub <tryevertth@gmail.com>
2024-10-10 19:44:41 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,24 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    experimental: {
-      ppr: 'incremental',
-    },
-  };
-
-export default nextConfig;
+// next.config.mjs
+export default {
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply these headers to all routes
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff', // Prevent MIME-type sniffing
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self'; object-src 'none';", // Adjust to your needs
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer', // Control the Referer header
+          },
+        ],
+      },
+    ];
+  },
+};
