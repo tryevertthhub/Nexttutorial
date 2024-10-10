922b606723225790ea1ee13dfc5f498bc10c4477
tryevertthhub <tryevertth@gmail.com>
2024-10-10 19:18:37 +0700

    Update layout.tsx
    update meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+       <head>
+        <meta name="zoom-domain-verification" content="ZOOM_verify_a4e3e9ce3c3c4b9ab0c1574abdf3bfc5" />
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
